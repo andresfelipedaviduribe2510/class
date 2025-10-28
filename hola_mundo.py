4fc169f160be3087c93bcbffe9f9871356724d33
Andres Felipe David Uribe <adaviduribe@gmail.com>
2025-10-28 10:45:39 -0500

    first commit

---
+++ hola_mundo.py
@@ -0,0 +1,12 @@
+
+
+nombre = input("Por favor ingresa tu nombre")
+edad = int(input("Ingrese la edad"))
+
+print(f"Mi nombre es: {nombre}")
+print(f"Mi edad es: {edad}")
+
+if edad >= 18:
+    print("Estas viejo")
+else:
+    print("Eres un sol")
